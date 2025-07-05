c9d751eb81fd6625c79de8db5a2ef67f8ee01492
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2025-07-05 13:45:39 +0530

    make direction change

--- first/script.js
+++ first/script.js
@@ -2,6 +2,7 @@
 const container_value = document.getElementById("Container_value");
 const Decrement_btn = document.getElementById("Decrement_btn");
 const Increment_btn = document.getElementById("Increment_btn");
+const Reset_btn = document.getElementById("Reset_btn");
 
 //set the initial value
 let count = 0; //because let is can be change value
@@ -18,4 +19,9 @@ Decrement_btn.addEventListener("click",function(){
     console.log("Counter after decrement:" + count);
 });
 
+Reset_btn.addEventListener("click", function(){
+    count = 0; //reset the count to 0
+    container_value.textContent = count; //update the displayed value
+    console.log("Counter reset to: " + count);
+})
 // Reset button functionality
