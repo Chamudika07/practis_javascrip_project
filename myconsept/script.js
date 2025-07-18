3bb20e150358a2cd164bb81f0d43ad15ed80bef3
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2025-07-18 11:18:20 +0530

    make data fetch function

--- myconsept/script.js
+++ myconsept/script.js
@@ -2,3 +2,26 @@ const quote_text = document.getElementById('quote_text');
 const quote_author = document.getElementById('quote_author');
 const new_quote_btn = document.getElementById('new_quote_btn');
 
+const API_URL = 'https://api.quotable.io/random';
+
+async function getNewQuote() {
+    try{
+        quote_text.textContent = 'Loading...'; // Show loading text
+        quote_author.textContent = ''; // Clear previous author
+
+        const response = await fetch(API_URL);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        quote_text.textContent = `"${data.content}"`
+        quote_author.textContent = `-${data.author}`;
+    }
+    catch (error){
+        console.error('Error fetching quote:', error);
+        quote_text.textContent = 'Failed to load quote.';
+        quote_author.textContent = '';
+    }
+}
