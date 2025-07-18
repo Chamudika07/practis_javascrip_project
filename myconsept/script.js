ec9d3e2591d92eb0d2dd181f6574cb0662d06f67
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2025-07-18 10:55:27 +0530

    start javascript and make variable

---
+++ myconsept/script.js
@@ -0,0 +1,4 @@
+const quote_text = document.getElementById('quote_text');
+const quote_author = document.getElementById('quote_author');
+const new_quote_btn = document.getElementById('new_quote_btn');
+
